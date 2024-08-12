9d7ed206c5916656575b0cd1b3fb714bfe3704eb
GalipYanar <qalipynrr@gmail.com>
2024-08-12 21:31:00 +0300

    ürünleri sepete ekleme eklendi

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const jwtToken = localStorage.getItem("jwtToken"); //login ekranında set ettiğimiz token ı burada çekip kullanıyoruz
 const BASE_PATH = "http://localhost:8080/"
+const BASE_IMAGE_PATH = "/Users/Admin/Desktop/asd/E-commerce/"
+let cartItems = [];
 
 async function fetchCategories(){
     const response = await fetch(BASE_PATH + "category", {
@@ -29,7 +31,7 @@ async function fetchProductByCategory(categoryId){
         }
 
         const data= await response.json();
-    
+        displayProducts(data)
     }catch(error){
         console.error("Error fetching products: ", error)
     }
@@ -45,6 +47,42 @@ function displayCategories(categories) {
         option.text = category.categoryEnum;
         categorySelect.appendChild(option);
     });
+}
+
+function displayProducts(products){
+    const productList = document.getElementById("productList");
+    productList.innerHTML ='';
+
+    products.forEach(product =>{
+        const productCard = document.createElement("div");
+        productCard.classList.add("co-md-6", "mb-4");
+
+        const productImage = document.createElement("img");
+        productImage.src = BASE_IMAGE_PATH + product.image
+        productImage.alt = product.name;
+        productImage.style.maxWidth = "150px";
+        productImage.style.maxHeight = "150px";
+
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+        cardBody.innerHTML = `
+            <h5 class="card-title">${product.name}</h5>
+            <p class="card-text">${product.price}</p>
+            <button class="btn btn-primary" onclick='addToCart(${JSON.stringify(product)})'>Add to Cart</button>
+        `;
+
+        productCard.appendChild(productImage);
+        productCard.appendChild(cardBody);
+
+        productList.appendChild(productCard);
+    });
+}
+
+function addToCart(product) {
+    if(product.unitInStock > 0) {
+        cartItems.push(product);
+        updateCart();
+    }
 }
 
 document.addEventListener("DOMContentLoaded", async function() {
